1133a88f955e9afdbf52cdec76235f2e3126afcd
Antonio Alexandre <a.a.a.j@hotmail.com.br>
2019-12-10 20:34:03 -0300

    alteracao

---
+++ lib/Paginas/MenuPrincipal/MenuPrincipal.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class MenuPrincipal extends StatefulWidget {
+  @override
+  _MenuPrincipalState createState() => _MenuPrincipalState();
+}
+
+class _MenuPrincipalState extends State<MenuPrincipal> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
