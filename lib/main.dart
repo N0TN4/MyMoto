b1aceaa4fa7f44f5998765abbe8805198eedbcfd
Antonio Alexandre <a.a.a.j@hotmail.com.br>
2019-12-10 20:43:13 -0300

    alteracoes3

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("My Moto"),
+          title: Text("My MotoOO"),
         ),
         body: Container());
   }
