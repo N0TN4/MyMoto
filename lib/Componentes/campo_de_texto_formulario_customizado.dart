a068b58e376ec6994bd0802ce233ec5c08ed7d5b
Antonio Alexandre <a.a.a.j@hotmail.com.br>
2019-12-10 23:26:22 -0300

    Criacao do componente de campo de texto (formulario)

---
+++ lib/Componentes/campo_de_texto_formulario_customizado.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CampoDeTextoFormularioCustomizado extends StatelessWidget {
+  final Key key;
+  final Widget preIcone;
+  final Widget posIcone;
+  final String rotulo;
+  final TextEditingController controlador;
+  final TextInputType tipoDoInput;
+  final bool ativo;
+  final FocusNode focusNode;
+  final Function campoSubmetido;
+  final int linhasMax;
+  final bool required;
+  final TextCapitalization capitalizacao;
+  final String valorInicial;
+  final String valorController;
+  final dynamic bloc;
+
+  CampoDeTextoFormularioCustomizado({
+    this.key,
+    this.preIcone,
+    this.posIcone,
+    this.rotulo,
+    this.controlador,
+    this.tipoDoInput,
+    this.ativo,
+    this.focusNode,
+    this.campoSubmetido,
+    this.linhasMax,
+    this.required,
+    this.capitalizacao,
+    this.valorInicial,
+    this.valorController,
+    this.bloc,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    var tema = Theme.of(context);
+
+    return TextFormField(
+      maxLines: linhasMax,
+      focusNode: focusNode,
+      textCapitalization:
+          capitalizacao == null ? TextCapitalization.none : capitalizacao,
+      onFieldSubmitted: campoSubmetido,
+      keyboardType: tipoDoInput,
+      controller: controlador,
+      validator: required == true
+          ? (value) => value.isEmpty ? 'campo obrigatorio' : null
+          : null,
+      decoration: InputDecoration(
+          labelStyle: TextStyle(height: 0.0, color: tema.primaryColor),
+          contentPadding:
+              EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
+          suffixIcon: posIcone,
+          prefixIcon: preIcone,
+          labelText: rotulo),
+    );
+  }
+}
--- lib/Paginas/MenuPrincipal/menu_principal.dart
+++ lib/Paginas/MenuPrincipal/menu_principal.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/cupertino.dart' as prefix0;
 import 'package:flutter/material.dart';
+import 'package:mymoto/Componentes/campo_de_texto_formulario_customizado.dart';
 
 class MenuPrincipal extends StatefulWidget {
   @override
@@ -14,7 +16,16 @@ class _MenuPrincipalState extends State<MenuPrincipal> {
         
       ),
       body: Container(
-        child: Text("Hell World"),
+        // conflito prefix 0
+        // verificar em Componentes -> CampoCampoDeTextoFormularioCustomizado
+        // ou ctrl + click botão esquerdo para ir direto pra classe criada
+        // seguir mesmo padrão de widget stateless para botões
+        
+        child: CampoDeTextoFormularioCustomizado(
+          rotulo: "Hello World",
+          required: true,
+          tipoDoInput: prefix0.TextInputType.number,
+        )
       ),
     );
   }
