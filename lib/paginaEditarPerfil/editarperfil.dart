4ef52406382ff49c6f3211c256384ff96235de23
Ollin Magno <41454237+ollinmagno@users.noreply.github.com>
2019-12-10 21:17:46 -0300

    classe editarperfil

---
+++ lib/paginaEditarPerfil/editarperfil.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class EditarPerfil extends StatefulWidget {
+  @override
+  _EditarPerfilState createState() => _EditarPerfilState();
+}
+
+class _EditarPerfilState extends State<EditarPerfil> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
