ad94c109c9d6f519c4c6ffd0cc77f5e2bc856106
Ollin Magno <41454237+ollinmagno@users.noreply.github.com>
2020-01-07 22:16:12 -0300

    AppBar da tela mensagens

--- lib/Paginas/Mensagens/tela_mensagens.dart
+++ lib/Paginas/Mensagens/tela_mensagens.dart
@@ -1,1 +1,25 @@
 import 'package:flutter/material.dart';
+
+class TelaMensagens extends StatefulWidget {
+  @override
+  _TelaMensagensState createState() => _TelaMensagensState();
+}
+
+class _TelaMensagensState extends State<TelaMensagens> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Mensagens",
+        ),
+        backgroundColor: Color.fromRGBO(186, 27, 29, 1),
+        actions: <Widget>[
+          InkWell(
+            child: SizedBox(width: 80, child: Icon(Icons.search)),
+          ),
+        ],
+      ),
+    );
+  }
+}
