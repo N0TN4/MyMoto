65ffab0185a44295a497b93f3edd560a1038e22e
Luiz Roberto <passageiro1829@gmail.com>
2020-01-21 22:50:15 -0300

    adicionando classes para bloc e firebase da tela cadastro por email

---
+++ lib/Paginas/Cadastro/servico_firebase_cadastro_por_email.dart
@@ -0,0 +1,21 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:mymoto/Modelos/usuario.dart';
+
+class ServicoFirebaseCadastroPorEmail {
+
+  salvar(Usuario usuario) {
+
+    Firestore.instance.collection("usuarios").document().setData({
+      'ilogn': usuario.login,
+      'nome': usuario.nome,
+      'senha': usuario.senha,
+      'email': usuario.email,
+      'confirmarSenha': usuario.confirmarSenha,
+      'telefone': usuario.telefone,
+      'modelo': usuario.moto.modelo,
+      'marca': usuario.moto.marca,
+      'quilometragem': usuario.moto.quiometragem,
+    });
+  }
+  
+}
