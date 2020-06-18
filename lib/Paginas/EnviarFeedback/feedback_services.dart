752503b71fce8abd923200edee9b78bb8e46a522
Ollin Magno <41454237+ollinmagno@users.noreply.github.com>
2020-06-18 18:50:45 -0300

    método post, enviar feedback

---
+++ lib/Paginas/EnviarFeedback/feedback_services.dart
@@ -0,0 +1,12 @@
+import 'package:mymoto/services/abstract_services.dart';
+import 'dart:convert' as json;
+
+class FeedbackServices extends AbstractService {
+  FeedBackServices() : super('/feedback');
+
+   @override
+  fromJson(json) {
+    
+  }
+
+}
