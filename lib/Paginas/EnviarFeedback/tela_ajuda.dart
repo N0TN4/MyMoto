643d6783107c33e325f193ba985ba94c7c9d07de
Ollin Magno <41454237+ollinmagno@users.noreply.github.com>
2020-06-18 18:36:01 -0300

    renomeando tela

---
+++ lib/Paginas/EnviarFeedback/tela_ajuda.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:mymoto/Paginas/EnviarFeedback/obter_informacoes_aparelho.dart';
+import 'package:mymoto/Paginas/EnviarFeedback/relatar_bug.dart';
+import 'package:mymoto/Paginas/EnviarFeedback/relatar_feedback.dart';
+
+
+class TelaAjuda extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Ajuda",
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.red,
+      ),
+      body: Container(
+        child: Column(
+          //
+          children: <Widget>[
+            ListTile(
+              title: Text("Enviar feedback"),
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => RelatarFeedback()));
+              },
+            ),
+            ListTile(
+              title: Text("Reporte um bug"),
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => RelatarBug()));
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
