08d639487a0b30fd313f00ed3c2e8ba4ea633181
Ollin Magno <41454237+ollinmagno@users.noreply.github.com>
2020-06-18 21:35:33 -0300

    método post enviar msg

--- lib/Paginas/EnviarFeedback/feedback_services.dart
+++ lib/Paginas/EnviarFeedback/feedback_services.dart
@@ -1,12 +1,32 @@
+import 'package:mymoto/Paginas/EnviarFeedback/obter_informacoes_aparelho.dart';
+import 'package:mymoto/Paginas/EnviarFeedback/relatar_feedback.dart';
 import 'package:mymoto/services/abstract_services.dart';
 import 'dart:convert' as json;
 
 class FeedbackServices extends AbstractService {
-  FeedBackServices() : super('/feedback');
+  FeedbackServices() : super('/feedback');
 
-   @override
+  Future<dynamic> enviarMensagem(RelatarFeedback relatar, InformacoesAparelho aparelho){
+    return Session.post('${AbstractService.staticAPI}/feedback',
+      body: toJsonFeedback(relatar, aparelho)).then((response){
+        print(response);
+        print("JSON POST: ${json.jsonEncode(toJsonFeedback(relatar, aparelho))}");
+      });
+  }
+  Map<String, dynamic> toJsonFeedback(RelatarFeedback relatar, InformacoesAparelho aparelho) => {
+    "mensagemDoUsuario" : relatar,
+    "dataDeEnvio" : relatar,
+    "informacoesDoAparelho" : aparelho.deviceData,
+  };
+
+  @override
   fromJson(json) {
-    
+    return json;
+  }
+
+  @override
+  fromJsonUsuario(json) {
+    return json;
   }
 
 }
