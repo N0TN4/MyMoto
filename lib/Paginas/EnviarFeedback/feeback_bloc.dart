99fa581d54f2d26429eea8df9eaa8608f99edbb3
Ollin Magno <41454237+ollinmagno@users.noreply.github.com>
2020-06-20 22:45:30 -0300

    bloc, metodo post

--- lib/Paginas/EnviarFeedback/feeback_bloc.dart
+++ lib/Paginas/EnviarFeedback/feeback_bloc.dart
@@ -1,13 +1,34 @@
+import 'package:mymoto/Paginas/EnviarFeedback/feedback_services.dart';
+import 'package:mymoto/Paginas/EnviarFeedback/feedback_model.dart';
 import 'package:rxdart/rxdart.dart';
 
 class FeedbackBloc {
-  final _mensagem = BehaviorSubject<String>();
+  final _mensagemDoUsuario = BehaviorSubject<String>();
+  
+  Function(String) get mensagemDoUsuario => _mensagemDoUsuario.sink.add;
 
-  Stream<String> get mensagem => _mensagem.stream;
+  FeedbackServices _services = new FeedbackServices();
+  
 
-  Function(String) get setMensagem => _mensagem.sink.add;
+  Future<bool> enviarMensagem() async {
+    FeedBackModel feedback = new FeedBackModel();
+    feedback.mensagemDoUsuario = _mensagemDoUsuario.value;
+    
+    print("${feedback.mensagemDoUsuario}");
+    return await _services.enviarMensagem(feedback).then((response) {
+      print("POST - $response");
+      if (response != null) {
+        
+        return true;
+      }
+      else{
+        return false;
+      }
+    });
 
-  dispose() {
-    _mensagem.close();
+  }
+
+  void dispose() {
+    _mensagemDoUsuario.close();
   }
 }
