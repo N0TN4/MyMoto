005ae291a150a1aec48beb210f7142849223d872
Luiz Roberto <passageiro1829@gmail.com>
2020-08-12 20:04:37 -0300

    adicionando mais motos

--- lib/Paginas/Cadastro/cadastro_services.dart
+++ lib/Paginas/Cadastro/cadastro_services.dart
@@ -49,6 +49,7 @@ class CadastroServices extends AbstractService {
       modelo: "CG",
       nome: "HONDA CG 160 FAN",
     );
+
     Moto moto2 = new Moto(
       id: 2,
       cilindradas: 125,
@@ -63,6 +64,7 @@ class CadastroServices extends AbstractService {
       modelo: "YBR",
       nome: "YAMAHA YBR 125i ED",
     );
+
     Moto moto3 = new Moto(
       id: 3,
       cilindradas: 125,
@@ -77,9 +79,121 @@ class CadastroServices extends AbstractService {
       modelo: "YES",
       nome: "SUZUKI YES EN125",
     );
+
+    Moto moto4 = new Moto(
+      id: 4,
+      cilindradas: 109,
+      kmMaxTrocaOleo: 6000,
+      kmMaxAcelerador: 6000,
+      kmMaxVela: 12000,
+      kmMaxFreio: 6000,
+      kmMaxEmbreagem: 6000,
+      kmMaxPneus: 1000,
+      kmMaxSuspensao: 24000,
+      marca: "HONDA",
+      modelo: "BIZ",
+      nome: "HONDA BIZ 110i",
+    );
+
+    Moto moto5 = new Moto(
+      id: 5,
+      cilindradas: 250,
+      kmMaxTrocaOleo: 3000,
+      kmMaxAcelerador: 3000,
+      kmMaxVela: 12000,
+      kmMaxFreio: 3000,
+      kmMaxEmbreagem: 3000,
+      kmMaxPneus: 1000,
+      kmMaxSuspensao: 12000,
+      marca: "HONDA",
+      modelo: "CBX",
+      nome: "HONDA CBX 250 TWISTER",
+    );
+
+    Moto moto6 = new Moto(
+      id: 6,
+      cilindradas: 160,
+      kmMaxTrocaOleo: 3000,
+      kmMaxAcelerador: 3000,
+      kmMaxVela: 10000,
+      kmMaxFreio: 3000,
+      kmMaxEmbreagem: 3000,
+      kmMaxPneus: 1000,
+      kmMaxSuspensao: 12000,
+      marca: "SUZUKI",
+      modelo: "DR",
+      nome: "SUZUKI DR 160",
+    );
+
+    Moto moto7 = new Moto(
+      id: 7,
+      cilindradas: 250,
+      kmMaxTrocaOleo: 6000,
+      kmMaxAcelerador: 6000,
+      kmMaxVela: 12000,
+      kmMaxFreio: 6000,
+      kmMaxEmbreagem: 6000,
+      kmMaxPneus: 1000,
+      kmMaxSuspensao: 12000,
+      marca: "KAWASAKI",
+      modelo: "NINJA",
+      nome: "KAWASAKI NINJA 250R",
+    );
+
+    Moto moto8 = new Moto(
+      id: 8,
+      cilindradas: 125,
+      kmMaxTrocaOleo: 5000,
+      kmMaxAcelerador: 5000,
+      kmMaxVela: 10000,
+      kmMaxFreio: 5000,
+      kmMaxEmbreagem: 5000,
+      kmMaxPneus: 1000,
+      kmMaxSuspensao: 12000,
+      marca: "YAMAHA",
+      modelo: "FACTOR",
+      nome: "YAMAHA FACTOR YBR 125",
+    );
+    Moto moto9 = new Moto(
+      id: 9,
+      cilindradas: 250,
+      kmMaxTrocaOleo: 5000,
+      kmMaxAcelerador: 5000,
+      kmMaxVela: 10000,
+      kmMaxFreio: 5000,
+      kmMaxEmbreagem: 5000,
+      kmMaxPneus: 1000,
+      kmMaxSuspensao: 12000,
+      marca: "YAMAHA",
+      modelo: "FAZER",
+      nome: "YAMAHA FAZER 250 ABS",
+    );
+
+    Moto moto10 = new Moto(
+      id: 10,
+      cilindradas: 160,
+      kmMaxTrocaOleo: 6000,
+      kmMaxAcelerador: 6000,
+      kmMaxVela: 12000,
+      kmMaxFreio: 6000,
+      kmMaxEmbreagem: 6000,
+      kmMaxPneus: 1000,
+      kmMaxSuspensao: 18000,
+      marca: "HONDA",
+      modelo: "NXR",
+      nome: "HONDA NXR 160 BROS",
+    );
+
     motos.add(moto1);
     motos.add(moto2);
     motos.add(moto3);
+    motos.add(moto4);
+    motos.add(moto5);
+    motos.add(moto6);
+    motos.add(moto7);
+    motos.add(moto8);
+    motos.add(moto9);
+    motos.add(moto10);
 
     return motos;
   }
